8fd05e18a904c8c6c7cf9dba618bbbf31e3189e6
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2024-01-30 22:45:14 +0330

    Add files via upload

---
+++ ConsoleApp170/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp170
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder1 = new StringBuilder("Happy Birthday Jane");
+            var builder2 = new StringBuilder("good bye greg");
+
+            Console.WriteLine($"Before replacements Inserts:\n{builder1}\n{builder2}");
+
+            builder1.Replace("Jane", "Greg");
+            builder2.Replace('g', 'G', 0, 5);
+
+            Console.WriteLine($"After replacements Removes:\n{builder1}\n{builder2}");
+
+            Console.ReadLine();
+        }
+    }
+}
